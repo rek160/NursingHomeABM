17995cd464887c2c87a705562d4b0286a54a3157
Rebecca Kahn <rek160@mail.harvard.edu>
2020-11-14 11:38:38 -0500

    update code

--- R0 calculation.R
+++ R0 calculation.R
@@ -1,6 +1,4 @@
 library(tidyverse)
-VL <- read_rds('./2020-09-2873_VL_master_simulations.rds')
-
 VL %>% 
   subset(State %in% c("A.hcwC", "A.hcwNC", "A.rNC", "I.hcwC", "I.hcwNC", "I.rC", "I.rNC" )) %>%
   subset(Testing=="none" & Intervention=="A) No intervention") %>%
@@ -12,23 +10,9 @@ VL %>%
 
 infectious_days %>%
   ungroup() %>%
-  summarise(mean_days=mean(I_days))-> mean_days
-mean_days
+  summarise(median_days=median(I_days))-> median_days
+median_days
 
-round(((1-(1-0.02)**10)*1 + (1-(1-0.02)**3)*6) * mean_days,2) # residents
-round(((1-(1-0.02)**3)*6 + 0.02*2) * mean_days,2) # staff
+round(((1-(1-0.02)**10)*1 + (1-(1-0.02)**3)*6) * median_days,2) # residents
+round(((1-(1-0.02)**3)*6 + 0.02*2) * median_days,2) # staff
 
-
-# View(VL %>% subset(ID==203 & Testing=="none" & Intervention=="A) No intervention" &
-#                      State %in% c("A.hcwC", "A.hcwNC", "A.rNC", "I.hcwC", "I.hcwNC", "I.rC", "I.rNC" )))
-# 
-# View(VL %>% subset(Testing=="none" & Intervention=="A) No intervention" &
-#                      State %in% c( "I.hcwC", "I.hcwNC", "I.rC", "I.rNC" )))
-# 
-# View(VL %>% subset(ID==1 & Testing=="daily_antigen" & Intervention=="B) Resident intervention" &
-#                      State %in% c("A.hcwC", "A.hcwNC", "A.rNC", "I.hcwC", "I.hcwNC", "I.rC", "I.rNC" )))
-# 
-# VL %>% 
-#   subset(State %in% c("A.hcwC", "A.hcwNC", "A.rNC", "I.hcwC", "I.hcwNC", "I.rC", "I.rNC" )) %>%
-#   group_by(ID,Testing,Intervention,Sim.day,State) %>%
-#   tally() -> temp
